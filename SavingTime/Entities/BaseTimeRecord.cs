20c276e0721bc24f2d337ae94396df278929eb43
Carlos Beralde (Caique) <carlosb@lacunasoftware.com>
2023-12-03 03:50:57 -0300

    remove second before saving in the db

--- SavingTime/Entities/BaseTimeRecord.cs
+++ SavingTime/Entities/BaseTimeRecord.cs
@@ -3,7 +3,26 @@
     public class BaseTimeRecord
     {
         public int Id { get; set; }
-        public DateTime Time { get; set; }
+        private DateTime _time;
+        public DateTime Time {
+            get {
+                return _time;
+            }
+            set {
+                if (value.Second >= 30) {
+                    value = value.AddMinutes(1);
+                    value = new DateTime(
+                        value.Year,
+                        value.Month,
+                        value.Day,
+                        value.Hour,
+                        value.Minute,
+                        0
+                    );
+                }
+                _time = value;
+            }
+        }
         public TimeRecordType Type { get; set; }
 
         public BaseTimeRecord(DateTime time, TimeRecordType type) {
