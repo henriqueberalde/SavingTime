b470e0fef30d9125e63160bc5d8f2c93e7bdd71a
Carlos Beralde (Caique) <carlosb@lacunasoftware.com>
2024-08-17 22:55:45 -0300

    Tretment of exception, Config command

---
+++ SavingTime/Bussiness/Commands/ConfigCommand.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SavingTime.Bussiness.Commands;
+using SavingTime.Data;
+using System.ComponentModel;
+
+namespace SavingTime.Bussiness
+{
+    [Verb("config", HelpText = "Show configuration values.")]
+    public class ConfigCommand : BaseCommand
+    {
+        public override void Run(IHost host, SavingTimeDbContext dbContext) {
+            var pontoConfig = host.Services.GetService<PontoConfiguration>();
+            var jiraConfig = host.Services.GetService<JiraConfiguration>();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("PontoConfig");
+            showProperties(pontoConfig);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("JiraConfig");
+            showProperties(jiraConfig);
+        }
+
+        private void showProperties(object obj) {
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+            {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(obj);
+                Console.WriteLine("  {0}: {1}", name, value);
+            }
+        }
+    }
+}
