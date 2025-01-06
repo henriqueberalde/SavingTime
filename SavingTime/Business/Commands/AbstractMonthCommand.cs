b90fda0d81e0078f7bfd57dc71daae9debdff948
Carlos Beralde (Caique) <carlosb@lacunasoftware.com>
2025-01-06 16:57:15 -0300

    Add file

---
+++ SavingTime/Business/Commands/AbstractMonthCommand.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+
+namespace SavingTime.Business.Commands;
+
+public abstract class AbstractMonthCommand: BaseCommand
+{
+    [Option('m', "month", Required = false, HelpText = "Month of records to show. Format MM or yyyy-MM")]
+    public string? Month { get; set; }
+    protected DateTime? DateRef
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(Month))
+            {
+                return null;
+            }
+
+            if (int.TryParse(Month, out var month))
+            {
+                return new DateTime(DateTime.Now.Year, month, 1);
+            }
+
+            var dateRefString = Month + "-01";
+
+            return DateTime.ParseExact(
+                dateRefString,
+                "yyyy-MM-dd",
+                System.Globalization.CultureInfo.InvariantCulture
+            );
+        }
+    }
+}
